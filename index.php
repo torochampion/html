65379e354a103af31d08ba6c2c3de3cc425799c0
torochampion <lprplpuser05@gmail.com>
2017-02-02 08:45:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo "<h1>PHASE 05 INSTRUCTION</h1>";
 echo "<h2>Setup a WORDPRESS</br>";
 echo "srv01 will serve as a database server</br>";
-echo "and srv02 will be a web server";
+echo "and srv02 will be a web server</br>";
 echo "Please PM on facebook whene you are done<h2>";
 ?>
